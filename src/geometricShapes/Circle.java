3fa6a87a34f3a863697875aa8fdcc5a9907d833c
Thayanne Luiza <thayannevls@gmail.com>
2015-08-24 21:24:58 -0300

    update circle

--- src/geometricShapes/Circle.java
+++ src/geometricShapes/Circle.java
@@ -1,5 +1,28 @@
+/**
+ * 
+ */
 package geometricShapes;
 
-public class Circle {
-
+public class Circle implements ShapeIF{
+	public static final double PI = 3.14 ;
+	private double radius;
+	
+	
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	public double getRadius(){
+		return radius;
+	}
+	
+	public void setRadius(double radius){
+		this.radius = radius;
+	}
+	public double perimeter() throws Exception{
+		return 2 * PI * getRadius();
+	}
+	
+	public double area() throws Exception{
+		return PI * getRadius() * getRadius();
+	}
 }
