409ab48a445ebc2be671656681295c436b2831d1
Rebeca Galdino <rebecaolg@gmail.com>
2015-08-25 14:32:54 -0300

    Update Square.java

--- src/geometricShapes/Square.java
+++ src/geometricShapes/Square.java
@@ -5,21 +5,20 @@ public class Square extends AbstractQuadrilateral{
 	
 	/**
 	 * @param side
-	 * @param height
 	 */
-	protected Square(double side, double height) {
-		super(side, height);
+	protected Square(double side) {
+		super(side, side);
 	}
 
 	public double perimeter() throws Exception{
-		if(side <= 0 || height <= 0)
+		if(side <= 0)
 			throw new Exception("Wrong value Typed. Should be greater than 0!");
 		else
-		return 2*side+2*height;
+		return 4*side;
 	}
 	
 	public double area()throws Exception{
-		if(side <= 0 || height <= 0)
+		if(side <= 0)
 			throw new Exception("Wrong value Typed. Should be greater than 0!");
 		else
 		return Math.pow(side,2);
